0c31774b9a3b906d463a48b8f3136cc65df0993a
vv87 <38062151+vv87@users.noreply.github.com>
2019-09-29 17:16:11 +0300

    Task https://stepik.org/lesson/201964/step/4?auth=registration&unit=176022 from Stepic

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,19 @@
 from pages.locators import ProductPageLocators
 from pages.product_page import ProductPage
-import time
+import pytest
 
-
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    # link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link)
     page.open()
     browser.find_element(*ProductPageLocators.SUBMIT_BUTTON).click()
@@ -14,4 +23,3 @@ def test_guest_can_add_product_to_basket(browser):
     page.check_text_book_in_basket()
     page.check_product_price_in_basket()
     page.should_be_success_of_addition_message()
-    time.sleep(5)
